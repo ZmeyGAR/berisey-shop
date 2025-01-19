406c292e3ec7e11c7b09b763c6837a38fe1c7e26
ZmeyGAR <lagutin1991@gmail.com>
2025-01-19 17:38:41 +0300

    инициализация проекта

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./berisey/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Montserrat", "serif", ...defaultTheme.fontFamily.sans],
+      serif: ["Montserrat", "serif", ...defaultTheme.fontFamily.sans]
+    },
+    extend: {
+      borderRadius: {
+        'large' : '48px'
+      },
+      screens: {
+        'sm-mobile': '360px',
+        'xl-mobile': '480px',
+        'sm-tablet': '640px',
+        'xl-tablet': '768px',
+        'sm-notebook': '1024px',
+        'xl-notebook': '1280px',
+        'sm-monitor': '1536px',
+        'xl-monitor': '1792px',
+      },
+      colors: {
+        white: '#FFFFFF',
+        lightBlack: "#313131",
+        lightGray: '#9294A1',
+        lightBlue: '#F6F7F8',
+        lightGreen: '#05D975',
+        darkGreen: '#04A65B',
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
