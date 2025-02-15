0972b5dd80fdf81289359b55a4de5b36f4fecb56
ZmeyGAR <lagutin1991@gmail.com>
2025-02-15 12:57:16 +0300

    новая структура папок

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    "./**/*.{html,js}",
+    "./berisey/**/*.{html,js}",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
